7a0f9c1acfa447fb36f6e952bd4f52287f63ae61
Fernanda <fernandacorredera@gmail.com>
2022-12-29 17:02:43 -0300

    Reduce: único valor

--- app/main.js
+++ app/main.js
@@ -2,7 +2,7 @@ let livros = []
 const endpointDaAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json'
 getBuscarLivrosDaAPI()
 
-async function getBuscarLivrosDaAPI() {
+async function getBuscarLivrosDaAPI(){
     const res = await fetch(endpointDaAPI)
     livros = await res.json()
     livros = aplicarDesconto(livros)
